5516fd4ed6162baefc427fca76438ff9b0962f7c
Jacob Patterson <jacob.patterson@gmail.com>
2018-11-05 16:19:46 -0800

    adding some basic grid code

--- src/com/github/ants280/slideGame/logic/Grid.java
+++ src/com/github/ants280/slideGame/logic/Grid.java
@@ -1,18 +1,169 @@
 package com.github.ants280.slideGame.logic;
 
+import java.util.Random;
 import java.util.stream.IntStream;
 
 public class Grid
 {
+	private static final boolean DEBUG = false;
 	private final int length;
 	private final Tile[][] rows;
 	private final Tile[][] cols;
+	private final Random random;
 
+	/**
+	 * Creates an empty, square grid of tiles.
+	 *
+	 * @param length The width and height of rows and columns in the grid.
+	 */
 	public Grid(int length)
 	{
+		if (length < 2)
+		{
+			throw new IllegalArgumentException("Length must be large enough "
+				+ "to slide tiles.  Found: " + length);
+		}
+		
 		this.length = length;
 		this.rows = createTiles(length);
 		this.cols = createTiles(length);
+		long seed = System.currentTimeMillis();
+		this.random = new Random(seed);
+
+		if (DEBUG)
+		{
+			System.out.printf(
+					"Created grid with length %d and seed %d%n", length, seed);
+		}
+	}
+
+	/**
+	 * Add a random tile to an empty spot on the grid.
+	 */
+	public void addRandomTile()
+	{
+		// This may be slow with large game lengths.
+		// TODO: this will infinite-loop if the grid is filled, so it is buggy.
+		int r, c;
+
+		Tile tile = random.nextInt(10) == 0 ? Tile.V_4 : Tile.V_2;
+		do
+		{
+			r = random.nextInt(length);
+			c = random.nextInt(length);
+		}
+		while (getTile(r, c) == null);
+
+		if (DEBUG)
+		{
+			System.out.printf("\tAdding %s Tile at [%d,%d]%n", tile, r, c);
+		}
+
+		setTile(r, c, tile);
+	}
+
+	public int slideTilesLeft()
+	{
+		if (DEBUG)
+		{
+			System.out.println("\tLEFT");
+		}
+
+		return slideRows(true);
+	}
+
+	public int slideTilesRight()
+	{
+		if (DEBUG)
+		{
+			System.out.println("\tRIGHT");
+		}
+
+		return slideRows(false);
+	}
+
+	public int slideTilesUp()
+	{
+		if (DEBUG)
+		{
+			System.out.println("\tUP");
+		}
+
+		return slideCols(true);
+	}
+
+	public int slideTilesDown()
+	{
+		if (DEBUG)
+		{
+			System.out.println("\tDOWN");
+		}
+
+		return slideCols(false);
+	}
+
+	private int slideRows(boolean towardZero)
+	{
+		return slideTiles(true, towardZero);
+	}
+
+	private int slideCols(boolean towardZero)
+	{
+		return slideTiles(true, towardZero);
+	}
+
+	private int slideTiles(boolean slideRows, boolean towardZero)
+	{
+		int sum = 0;
+
+		for (int i = 0; i < length; i++)
+		{
+			sum += slideTiles(i, slideRows, towardZero);
+		}
+
+		return sum;
+	}
+
+	private int slideTiles(int index, boolean slideRows, boolean towardZero)
+	{
+		Tile[] slidTiles = new Tile[length];
+		
+		int sum = 0;
+		
+		for (int i = 0; i + 1 < length; i++)
+		{
+			//TODO
+		}
+
+		return sum;
+	}
+
+	// package-private for testing
+	void setTile(int r, int c, Tile tile)
+	{
+		if (DEBUG)
+		{
+			System.out.printf("Setting tile at [%d,%d] to %s%n", r, c, tile);
+		}
+
+		rows[r][c] = tile;
+		cols[c][r] = tile;
+	}
+
+	public Tile getTile(int r, int c)
+	{
+		if (DEBUG)
+		{
+			if (rows[r][c] != cols[c][r])
+			{
+				System.err.printf(
+						"ERROR: Same tile should be saved in rotated data "
+						+ "structure at r=%d,c=%d.  Found %s and %s.%n",
+						r, c, rows[r][c], cols[c][r]);
+			}
+		}
+
+		return rows[r][c];
 	}
 
 	private static Tile[][] createTiles(int length)
@@ -21,40 +172,5 @@ public class Grid
 				.map(i -> length)
 				.mapToObj(Tile[]::new)
 				.toArray(Tile[][]::new);
-	}
-
-	public boolean slideTilesLeft()
-	{
-		return slideRows(true);
-	}
-
-	public boolean slideTilesRight()
-	{
-		return slideRows(false);
-	}
-
-	public boolean slideTilesUp()
-	{
-		return slideCols(true);
-	}
-
-	public boolean slideTilesDown()
-	{
-		return slideCols(false);
-	}
-
-	boolean slideRows(boolean slideTowardStart)
-	{
-		return slideTiles(true, slideTowardStart);
-	}
-
-	boolean slideCols(boolean slideTowardStart)
-	{
-		return slideTiles(true, slideTowardStart);
-	}
-
-	private boolean slideTiles(boolean slideRows, boolean slideTowardStart)
-	{
-		throw new UnsupportedOperationException("Not supported yet.");
 	}
 }
---
+++ test/com/github/ants280/slideGame/logic/GridTest.java
@@ -0,0 +1,100 @@
+package com.github.ants280.slideGame.logic;
+
+import junit.framework.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class GridTest
+{
+	private static final int LENGTH = 4;
+	private Grid grid;
+	
+	@Before
+	public void setUp()
+	{
+		grid = new Grid(LENGTH);
+	}
+	
+	@Test
+	public void testInitialGridEmpty()
+	{
+		for (int r = 0; r < LENGTH; r++)
+		{
+			for (int c = 0; c < LENGTH; c++)
+			{
+				Assert.assertNull(
+						String.format("Non-null tile found at [%d,%d]", r, c),
+						grid.getTile(r, c));
+			}
+		}
+	}
+	
+	@Test
+	public void testSlideTilesLeft()
+	{
+		grid.setTile(1, 1, Tile.V_2);
+		grid.slideTilesLeft();
+		
+		Assert.assertEquals(Tile.V_2, grid.getTile(1, 0));
+		Assert.assertNull(grid.getTile(1, 1));
+	}
+
+	@Test
+	public void testSlideTilesRight()
+	{
+		grid.setTile(1, 1, Tile.V_2);
+		grid.slideTilesLeft();
+		
+		Assert.assertEquals(Tile.V_2, grid.getTile(3, 0));
+		Assert.assertNull(grid.getTile(1, 1));
+	}
+
+	@Test
+	public void testSlideTilesUp()
+	{
+		grid.setTile(1, 1, Tile.V_2);
+		grid.slideTilesLeft();
+		
+		Assert.assertEquals(Tile.V_2, grid.getTile(0, 1));
+		Assert.assertNull(grid.getTile(1, 1));
+	}
+
+	@Test
+	public void testSlideTilesDown()
+	{
+		grid.setTile(1, 1, Tile.V_2);
+		grid.slideTilesLeft();
+		
+		Assert.assertEquals(Tile.V_2, grid.getTile(3, 1));
+		Assert.assertNull(grid.getTile(1, 1));
+	}
+
+	@Test
+	public void testSlideTiles_0202()
+	{
+		grid.setTile(0, 1, Tile.V_2);
+		grid.setTile(0, 3, Tile.V_2);
+		int moveScore = grid.slideTilesLeft();
+		
+		Assert.assertEquals(4, moveScore);
+		Assert.assertEquals(Tile.V_4, grid.getTile(0, 0));
+		Assert.assertNull(grid.getTile(0, 1));
+		Assert.assertNull(grid.getTile(0, 3));
+	}
+	
+	@Test
+	public void testSlideTiles_2244()
+	{
+		grid.setTile(0, 0, Tile.V_2);
+		grid.setTile(0, 1, Tile.V_2);
+		grid.setTile(0, 2, Tile.V_4);
+		grid.setTile(0, 3, Tile.V_4);
+		int moveScore = grid.slideTilesLeft();
+		
+		Assert.assertEquals(4, moveScore);
+		Assert.assertEquals(Tile.V_4, grid.getTile(0, 0));
+		Assert.assertEquals(Tile.V_8, grid.getTile(0, 1));
+		Assert.assertNull(grid.getTile(0, 2));
+		Assert.assertNull(grid.getTile(0, 3));
+	}
+}
