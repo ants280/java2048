6ee62335052c24e36fb754e7176355ac423db2c1
Jacob Patterson <jacob.patterson@gmail.com>
2018-11-07 15:26:57 -0800

    adding help and about popups

--- src/com/github/ants280/slideGame/ui/SlideGameFrame.java
+++ src/com/github/ants280/slideGame/ui/SlideGameFrame.java
@@ -12,6 +12,7 @@ import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.KeyStroke;
 import javax.swing.border.Border;
@@ -67,18 +68,61 @@ public class SlideGameFrame extends JFrame
 		exit_MI.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_X, KeyEvent.CTRL_DOWN_MASK));
 		exit_MI.addActionListener(actionEvent -> Runtime.getRuntime().exit(0));
 
+		JMenuItem help_MI = new JMenuItem("Help", KeyEvent.VK_H);
+		help_MI.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_H, KeyEvent.CTRL_DOWN_MASK));
+		help_MI.addActionListener(actionEvent -> this.showHelpPopup());
+
+		JMenuItem about_MI = new JMenuItem("About", KeyEvent.VK_F1);
+		about_MI.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_F1, KeyEvent.CTRL_DOWN_MASK));
+		about_MI.addActionListener(actionEvent -> this.showAboutPopup());
+
 		JMenu actionMenu = new JMenu("Action");
+		// TODO: set size, goal sliders
+		//actionMenu.add(setGridSize_MI);
+		//actionMenu.add(setGoalTile_MI);
+		//actionMenu.addSeparator();
 		actionMenu.add(newGame_MI);
 		actionMenu.add(exit_MI);
 
-		// TODO: Add help menu
-//		JMenu helpMenu = new JMenu("Help");
-//		helpMenu.add(help_MI);
-//		helpMenu.add(about_MI);
+		JMenu helpMenu = new JMenu("Help");
+		helpMenu.add(help_MI);
+		helpMenu.add(about_MI);
+
 		JMenuBar mainMenu = new JMenuBar();
 		mainMenu.add(actionMenu);
-//		mainMenu.add(helpMenu);
+		mainMenu.add(helpMenu);
 
 		return mainMenu;
+	}
+
+	private void showAboutPopup()
+	{
+		String message
+				= "By: Jacob Patterson"
+				+ "\n"
+				+ "\nCopyright(©) 2018"
+				+ "\n"
+				+ "\nBased on the game by Gabriele Cirulli.";
+		String title = "About " + getTitle();
+		showPopup(message, title, JOptionPane.INFORMATION_MESSAGE);
+	}
+
+	private void showHelpPopup()
+	{
+		String message
+				= "Slide the tiles all to the top, bottom, left, and right of the grid to\n"
+				+ "combine tiles of equal values. When tiles combine, their combined value is\n"
+				+ "the some of the value. Combine tiles by typing the appropriate arrow key,\n"
+				+ "using WSAD controls, or clicking and dragging in the desired direction on the\n"
+				+ "grid. After each turn a new tile is added. WThe game is over if the grid is\n"
+				+ "full and no moves can be made and is won when the 2048 tile is created.";
+
+		String title = "Help for " + getTitle();
+		showPopup(message, title, JOptionPane.QUESTION_MESSAGE);
+	}
+
+	private void showPopup(String message, String title, int messageType)
+	{
+		JOptionPane.showMessageDialog(this, message, title, messageType);
 	}
 }
