7576dd572934d45da4b855f41cc05e40991ea34c
Jacob Patterson <jacob.patterson@gmail.com>
2018-11-08 17:04:47 -0800

    locking aspect ratio of the grid component painting so tiles are always square.

--- src/com/github/ants280/slideGame/ui/SlideGameDisplayComponent.java
+++ src/com/github/ants280/slideGame/ui/SlideGameDisplayComponent.java
@@ -39,59 +39,64 @@ public class SlideGameDisplayComponent extends JComponent
 		tileFont = tileFont.deriveFont((float) fontSize);
 		g.setFont(tileFont);
 
-		// TODO: Ensure width = height.
 		int width = this.getWidth();
 		int height = this.getHeight();
+		int minDimension = Math.min(width, height);
+		double xOffset = (width - minDimension) / 2d;
+		double yOffset = (height - minDimension) / 2d;
 		int gridLength = grid.getLength();
-		double tilePercentage = 0.90d;
-		double colWidth = width / gridLength;
-		double rowHeight = height / gridLength;
-		// Painting may leave gaps if the tile width/height are not rounded.
-		double tileColWidth = round(colWidth * tilePercentage);
-		double tileRowHeight = round(rowHeight * tilePercentage);
-		double spacerColWidth = colWidth - tileColWidth;
-		double spacerRowHeight = rowHeight - tileRowHeight;
-		double halfSpacerColWidth = spacerColWidth / 2d;
-		double halfSpacerRowHeight = spacerRowHeight / 2d;
+		double cellSize = minDimension / gridLength;
+		// Painting may leave gaps between tiles and spacers if not rounded.
+		double tileSize = round(cellSize * 0.90d);
+		double spacerSize = cellSize - tileSize;
+		double halfSpacerSize = spacerSize / 2d;
 
 		paintGrid(
 				gridLength,
 				g,
-				width, height,
-				colWidth, rowHeight,
-				spacerColWidth, spacerRowHeight,
-				halfSpacerColWidth, halfSpacerRowHeight);
+				xOffset,
+				yOffset,
+				minDimension,
+				cellSize,
+				spacerSize,
+				halfSpacerSize);
 		paintTiles(
 				gridLength,
 				g,
-				colWidth, rowHeight,
-				tileColWidth, tileRowHeight,
-				halfSpacerColWidth, halfSpacerRowHeight);
+				xOffset,
+				yOffset,
+				cellSize,
+				tileSize,
+				halfSpacerSize);
 	}
 
 	private void paintGrid(
 			int gridLength,
 			Graphics g,
-			int width, int height,
-			double colWidth, double rowHeight,
-			double spacerColWidth, double spacerRowHeight,
-			double halfSpacerColWidth, double halfSpacerRowHeight)
+			double xOffset,
+			double yOffset,
+			double minDimension,
+			double cellSize,
+			double spacerSize,
+			double halfSpacerSize)
 	{
 		g.setColor(SlideGameColors.SPACER_COLOR);
 		for (int c = 0; c <= gridLength; c++)
 		{
-			int x = round((c * colWidth) - halfSpacerColWidth);
-			int y = 0;
-			int verticalLineWidth = round(spacerColWidth);
+			int x = round(xOffset + (c * cellSize) - halfSpacerSize);
+			int y = round(yOffset);
+			int verticalLineWidth = round(spacerSize);
+			int height = round(minDimension);
 			g.fillRect(
 					x, y,
 					verticalLineWidth, height);
 		}
 		for (int r = 0; r <= gridLength; r++)
 		{
-			int x = 0;
-			int y = round((r * rowHeight) - halfSpacerRowHeight);
-			int horizontalLineHeight = round(spacerRowHeight);
+			int x = round(xOffset);
+			int y = round(yOffset + (r * cellSize) - halfSpacerSize);
+			int width = round(minDimension);
+			int horizontalLineHeight = round(spacerSize);
 			g.fillRect(
 					x, y,
 					width, horizontalLineHeight);
@@ -101,9 +106,11 @@ public class SlideGameDisplayComponent extends JComponent
 	private void paintTiles(
 			int gridLength,
 			Graphics g,
-			double colWidth, double rowHeight,
-			double tileColWidth, double tileRowHeight,
-			double halfSpacerColWidth, double halfSpacerRowHeight)
+			double xOffset,
+			double yOffset,
+			double cellSize,
+			double tileSize,
+			double halfSpacerSize)
 	{
 		for (int c = 0; c < gridLength; c++)
 		{
@@ -111,20 +118,26 @@ public class SlideGameDisplayComponent extends JComponent
 			{
 				paintTile(
 						g,
-						c, r,
-						colWidth, rowHeight,
-						tileColWidth, tileRowHeight,
-						halfSpacerColWidth, halfSpacerRowHeight);
+						c,
+						r,
+						xOffset,
+						yOffset,
+						cellSize,
+						tileSize,
+						halfSpacerSize);
 			}
 		}
 	}
 
 	private void paintTile(
 			Graphics g,
-			int c, int r,
-			double colWidth, double rowHeight,
-			double tileColWidth, double tileRowHeight,
-			double halfSpacerColWidth, double halfSpacerRowHeight)
+			int c,
+			int r,
+			double xOffset,
+			double yOffset,
+			double cellSize,
+			double tileSize,
+			double halfSpacerSize)
 	{
 		Tile tile = grid.getTile(c, r);
 		if (tile != null)
@@ -132,46 +145,58 @@ public class SlideGameDisplayComponent extends JComponent
 			paintTileBackground(
 					SlideGameColors.getColor(tile),
 					g,
-					c, r,
-					colWidth, rowHeight,
-					tileColWidth, tileRowHeight,
-					halfSpacerColWidth, halfSpacerRowHeight);
+					c,
+					r,
+					xOffset,
+					yOffset,
+					cellSize,
+					tileSize,
+					halfSpacerSize);
 			paintTileText(
 					tile.getDisplayValue(),
 					g,
-					c, r,
-					colWidth, rowHeight);
+					c,
+					r,
+					xOffset,
+					yOffset,
+					cellSize);
 		}
 	}
 
 	private void paintTileBackground(
 			Color tileColor,
 			Graphics g,
-			int c, int r,
-			double colWidth, double rowHeight,
-			double tileColWidth, double tileRowHeight,
-			double halfSpacerColWidth, double halfSpacerRowHeight)
+			int c,
+			int r,
+			double xOffset,
+			double yOffset,
+			double cellSize,
+			double tileSize,
+			double halfSpacerSize)
 	{
 		g.setColor(tileColor);
-		int x = round((c * colWidth) + halfSpacerColWidth);
-		int y = round((r * rowHeight) + halfSpacerRowHeight);
-		int width = round(tileColWidth);
-		int height = round(tileRowHeight);
+		int x = round(xOffset + (c * cellSize) + halfSpacerSize);
+		int y = round(yOffset + (r * cellSize) + halfSpacerSize);
+		int width = round(tileSize);
+		int height = round(tileSize);
 		g.fillRect(x, y, width, height);
 	}
 
 	private void paintTileText(
 			String tileText,
 			Graphics g,
-			int c, int r,
-			double colWidth, double rowHeight)
+			int c,
+			int r,
+			double xOffset,
+			double yOffset,
+			double cellSize)
 	{
 		g.setColor(SlideGameColors.TILE_TEXT_COLOR);
 		double fontHeightPx = tileFont.getSize() * 0.75d;
 		FontMetrics fontMetrics = g.getFontMetrics();
 		int textWidth = fontMetrics.stringWidth(tileText);
-		int x = round((((c + 0.5d) * colWidth) - (textWidth / 2)));
-		int y = round((((r + 0.5d) * rowHeight) + (fontHeightPx / 2)));
+		int x = round(xOffset + (((c + 0.5d) * cellSize) - (textWidth / 2)));
+		int y = round(yOffset + (((r + 0.5d) * cellSize) + (fontHeightPx / 2)));
 		g.drawString(tileText, x, y);
 	}
 
