8eb2f5805046956b2b54bf42af344cf90ae13421
Jacob Patterson <jacob.patterson@gmail.com>
2018-11-28 19:24:15 -0800

    logging setting of look-and-feel failure instead of rethrowing it.

--- src/main/java/com/github/ants280/slideGame/ui/SlideGame.java
+++ src/main/java/com/github/ants280/slideGame/ui/SlideGame.java
@@ -1,7 +1,8 @@
 package com.github.ants280.slideGame.ui;
 
 import java.awt.Window;
-import javax.swing.JOptionPane;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
@@ -49,7 +50,11 @@ public class SlideGame implements Runnable
 				| IllegalAccessException
 				| UnsupportedLookAndFeelException ex)
 		{
-			JOptionPane.showMessageDialog(null, ex.getMessage());
+			Logger.getLogger(Sudoku.class.getName())
+					.log(
+							Level.SEVERE,
+							"Could not set system Look-And-Feel",
+							ex);
 		}
 	}
 }
